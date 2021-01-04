98e1b018216fee3f13ee23041fd004b2c1351361
stanbychkov <stanbychkov@gmail.com>
2021-01-04 14:50:11 +0300

    readme написан. Docstring комментарий к view-функция

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # hw05_final
+Социальная сеть, позволяющая публиковать личные посты. Имеет подписки, группы
+
+Running this project
+This project requires Python version 3.8.
+
+Install the project dependencies with
+
+`pip install -r requirements.txt`
+
+Synchronize the database state with the current set of models and migrations.
+
+`python manage.py migrate`
+
+Now you can run the project with this command
+
+`python manage.py runserver`
--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,8 @@ from .forms import PostForm, CommentForm
 
 
 def index(request):
+    """Старотовая страница"""
+
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
@@ -19,6 +21,8 @@ def index(request):
 
 
 def group_posts(request, slug):
+    """Сраница группы"""
+
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all()
     paginator = Paginator(post_list, 10)
@@ -33,6 +37,8 @@ def group_posts(request, slug):
 
 @login_required
 def new_post(request):
+    """Создание нового поста"""
+
     form = PostForm(request.POST or None, files=request.FILES or None)
     if form.is_valid():
         form.instance.author = request.user
@@ -42,6 +48,8 @@ def new_post(request):
 
 
 def profile(request, username):
+    """Страница профиля"""
+
     author = get_object_or_404(User, username=username)
     user_posts = author.posts.all()
     count_posts = author.posts.count()
@@ -60,6 +68,8 @@ def profile(request, username):
 
 
 def post_view(request, username, post_id):
+    """Страница просмотра отдельного поста"""
+
     user_post = get_object_or_404(Post, author__username=username, pk=post_id)
     author = get_object_or_404(User, username=username)
     count_posts = author.posts.count()
@@ -81,6 +91,8 @@ def post_view(request, username, post_id):
 
 @login_required()
 def post_edit(request, username, post_id):
+    """Страница редактирования поста"""
+
     post = get_object_or_404(Post, author__username=username, pk=post_id)
     if request.user != post.author:
         return redirect('post', username=username, post_id=post_id)
@@ -95,6 +107,8 @@ def post_edit(request, username, post_id):
 
 @login_required()
 def add_comment(request, username, post_id):
+    """Добавление комментария"""
+
     post = get_object_or_404(Post, author__username=username, pk=post_id)
     author = get_object_or_404(User, username=username)
     form = CommentForm(request.POST or None)
@@ -122,6 +136,8 @@ def add_comment(request, username, post_id):
 
 @login_required()
 def follow_index(request):
+    """Страница постов из подписок"""
+
     follower_post = Post.objects.filter(author__following__user=request.user)
     paginator = Paginator(follower_post, 10)
     page_number = request.GET.get('page')
@@ -131,6 +147,8 @@ def follow_index(request):
 
 @login_required()
 def profile_follow(request, username):
+    """Функция создания подписки на пользователя"""
+
     following = get_object_or_404(User, username=username)
     if following != request.user:
         Follow.objects.get_or_create(user=request.user, author=following)
@@ -139,6 +157,7 @@ def profile_follow(request, username):
 
 @login_required()
 def profile_unfollow(request, username):
+    """Функция отписки от пользователя"""
     following = get_object_or_404(User, username=username)
     if following != request.user:
         Follow.objects.filter(user=request.user, author=following).delete()
